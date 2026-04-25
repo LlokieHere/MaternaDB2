2f65ce0de29ef5ca0b6f46b0da3845da9f1e2d35
Kris <lokiepandak@gmail.com>
2026-04-25 10:33:30 +0800

    made another changes

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def sumz(a, b):
+    return a + b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Bitch')
+    print(sumz(2,6))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
